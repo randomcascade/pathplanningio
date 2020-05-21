68b81b8ad1b28f45ad6cd773651597f9c7f54157
randomcascade <goode.cameron@yahoo.com>
2020-05-20 23:00:29 -0400

    Made Static Grid
    Added the static grid which will be used to represent the search. Currently it is set maually to 20x20 at 20 pixels. Containers and size queries are still to be added. This was implemented in the p5.js library for simplicity.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RectanglesComponent } from './rectangles/rectangles.component';
 
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'rectangles', component: RectanglesComponent },
+  { path: '', redirectTo: '/rectangles', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/rectangles/rectangles.component.ts
+++ src/app/rectangles/rectangles.component.ts
@@ -1,4 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import * as p5 from 'p5';
+
+
+function make2DArray(row, columns) {
+  let temp = new Array(row);
+  for (let i = 0; i < row; i++) {
+    temp[i] = new Array(columns);
+  }
+  return temp;
+}
+
+class Cell {
+  public p: p5;
+  public i: number;
+  public x: number;
+  public y: number;
+  public size=20;
+  public isStart: boolean;
+  public isTarget: boolean;
+  public isBoundary: boolean;
+  public wasVisited: boolean;
+  public isObstacle: boolean;
+  constructor(s: p5, idx ?: number, row ?: number, col ?: number) {
+    this.i = idx;
+    this.x = row;
+    this.y = col;
+    this.p = s;
+  }
+  show() {
+    this.p.stroke(0);
+    this.p.fill(100);
+    this.p.rect(this.x, this.y, this.size, this.size);
+  }
+
+
+}
+
 
 @Component({
   selector: 'app-rectangles',
@@ -9,14 +46,31 @@ export class RectanglesComponent implements OnInit {
   public height = 10;
   public width = 10;
   public grid: any[];
-  constructor() {
-   }
+  canvas: any;
+  constructor() {}
 
   ngOnInit(): void {
-  }
-  newRectangles(h: number, w: number) {
-    this.grid = Array(h * w).fill(null);
+    const sketch = (s: p5) => {
+      s.setup = () => {
+        let canvas2 = s.createCanvas(401, 401);
+        canvas2.parent('grid-holder');
+        s.rect(0, 0, s.width, s.height);
+        s.stroke(2);
+      };
+      s.draw = () => {
+        s.background(10);
+        this.grid = make2DArray(20, 20);
+        for(let i = 0; i < 400; i++) {
+          this.grid[Math.floor(i/20)][i%20]= new Cell(s, i, Math.floor(i/20)*20, (i%20)*20);
+        }
+        for(let i = 0; i < 400; i++) {
+          this.grid[Math.floor(i/20)][i%20].show();
+        }
 
+
+      };
+    };
+    this.canvas = new p5(sketch);
   }
 
 }
